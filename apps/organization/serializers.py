959c69eea2834a1e8e601feea3cd96eb3eb44b5e
sara-poker <spoker1412@gmail.com>
2025-12-25 16:41:31 +0330

    add organization app

--- apps/organization/serializers.py
+++ apps/organization/serializers.py
@@ -1,3 +1,1 @@
 from rest_framework.serializers import BaseSerializer
-from rest_framework import serializers
-
---
+++ apps/organization/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import (TemplateView)
+from django.contrib.auth import get_user_model
+
+from web_project import TemplateLayout
+
+
+
