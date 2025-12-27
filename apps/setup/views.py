17248052f733401c6715ab24db708a9a57344121
sara-poker <spoker1412@gmail.com>
2025-12-27 12:07:20 +0330

    form add Team

--- apps/setup/views.py
+++ apps/setup/views.py
@@ -1,3 +1,5 @@
+from django.db.models import ProtectedError
+from django.shortcuts import redirect
 from django.views.generic import (TemplateView)
 from django.contrib.auth import get_user_model
 
@@ -12,8 +14,42 @@ class TeamView(TemplateView):
 
         teams = Team.objects.all()
 
+        context['class_notification'] = self.request.GET.get('alert_class', 'none_alert_mo')
+        context['message'] = self.request.GET.get('message', '')
         context['teams'] = teams
         return context
+
+    def post(self, request, *args, **kwargs):
+        # حذف سرور (اگر فرم حذف ارسال شده باشه)
+        if 'delete_team_id' in request.POST:
+            team_id = request.POST.get('delete_team_id')
+            try:
+                Team.objects.get(id=team_id).delete()
+                return redirect(f"{request.path}?alert_class=success_alert_mo&message=تیم با موفقیت حذف شد")
+            except ProtectedError:
+                return redirect(
+                    f"{request.path}?alert_class=err_alert_mo&message=برای این تیم پروژه و تسک هایی تعریف شده است، برای حذف تیم ابتدا پروژه های آن را به سایر تیم ها منتقل کنید.")
+
+        # اضافه کردن سرور جدید
+        name = request.POST.get('team_name', '').strip()
+        parent_team = request.POST.get('parent_team')
+
+        if not name:
+            return redirect(f"{request.path}?alert_class=err_alert_mo&message=لطفاً فیلد نام را پر کنید.")
+
+        if Team.objects.filter(name=name).exists():
+            return redirect(f"{request.path}?alert_class=err_alert_mo&message=نام تیم تکراری است")
+
+        if parent_team == 0 or parent_team == "0" or not parent_team:
+            parent_team = None
+
+        Team.objects.create(
+            name=name,
+            parent_id=parent_team,
+        )
+
+        return redirect(f"{request.path}?alert_class=success_alert_mo&message=تیم با موفقیت ثبت شد")
+
 
 class ProfileView(TemplateView):
     def get_context_data(self, **kwargs):
@@ -32,7 +68,6 @@ class UserDetailView(TemplateView):
         User = get_user_model()
         user = User.objects.filter(id=self.kwargs['pk'])
 
-
         context['user'] = user[0]
         context['device_info_list'] = []
         context['network_info_list'] = []
@@ -49,5 +84,3 @@ class UsersTableView(TemplateView):
 
         context['users'] = users
         return context
-
-
